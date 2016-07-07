15d78d63a404f558bc47e1ca6186b356f5cba56d
noraworld <noraworld@mail.com>
2016-07-07 18:10:50 +0100

    :sparkles: Create RESTful

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get '/articles', to: redirect('/')
+  root 'articles#index'
+  resources :articles
+  # get 'articles/index'
+  # get 'articles/show'
+  # get 'articles/new'
+  # get 'articles/create'
+  # get 'articles/edit'
+  # get 'articles/update'
+  # get 'articles/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
