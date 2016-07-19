3f3f1725b14bd302525c40a905afe85920cb9571
noraworld <noraworld@mail.com>
2016-07-19 19:47:50 +0100

    :fire: Delete the JavaScript library of the transform => Use CSS animation

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,17 +1,5 @@
 $(function() {
 
-  $('.post-box').on('mouseover', function() {
-    $(this).css('z-index', 2).css('border', '2px solid white');
-    $(this).stop().animate({
-      scale: [1.15, 1.15]
-    }, 50);
-  }).on('mouseout', function() {
-    $(this).css('z-index', 1);
-    $(this).stop().animate({
-      scale: [1.0, 1.0]
-    }, {duration: 50, complete: function() {$(this).css('border', '2px solid rgba(255, 255, 255, 0)'); }});
-  });
-
   $('#publish-link, #update-link').on('click', function() {
     $('#post-button').click();
   });
