f2ad125220ae14c5f577d6de5c307e461da11576
noraworld <noraworld@mail.com>
2016-09-16 13:16:53 +0100

    :recycle: Add comments

--- app/assets/javascripts/common/articles.js
+++ app/assets/javascripts/common/articles.js
@@ -1,9 +1,12 @@
 $(function() {
 
+  // 画像を囲む画像リンクを削除する
   $('a').children('img').unwrap();
 
+  // YouTubeの埋め込みタグをdivで囲む
   $('iframe').wrap('<div class="video-player-wrapper">');
 
+  // *.noraworld.jpは現在のタブで開きリファラを有効にする
   var domain = location.hostname.match(/^(.*?)([a-z0-9][a-z0-9\-]{1,63}\.[a-z\.]{2,6})[\:[0-9]*]?([\/].*?)?$/i)[2];
   $('a[href*="' + domain + '"]').removeAttr('target rel');
 
