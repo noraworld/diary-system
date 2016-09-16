b82e7291052df46b219f949938cd9b9b4a4c70ca
noraworld <noraworld@mail.com>
2016-09-16 13:12:58 +0100

    :pencil2: Allow to embed script element

--- app/assets/javascripts/common/articles.js
+++ app/assets/javascripts/common/articles.js
@@ -2,6 +2,9 @@ $(function() {
 
   $('a').children('img').unwrap();
 
+  $('iframe').wrap('<div class="video-player-wrapper">');
+
   var domain = location.hostname.match(/^(.*?)([a-z0-9][a-z0-9\-]{1,63}\.[a-z\.]{2,6})[\:[0-9]*]?([\/].*?)?$/i)[2];
   $('a[href*="' + domain + '"]').removeAttr('target rel');
+
 });
