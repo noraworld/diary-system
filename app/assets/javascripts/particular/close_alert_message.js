3948cda809dbee9954947cb6786038329e6c8b7e
noraworld <noraworld@mail.com>
2016-09-26 10:01:39 +0100

    :pencil2: Append keydown event to press esc key to close alert message

--- app/assets/javascripts/particular/close_alert_message.js
+++ app/assets/javascripts/particular/close_alert_message.js
@@ -1,9 +1,21 @@
 $(function() {
 
+  // Click a cross button to close.
   $('#times').on('click', function() {
     $(this).parent().fadeOut(600, function() {
       $(this).remove();
     });
+  });
+  // Press esc key to close.
+  $(window).on('keydown', function(event) {
+    if ( !($('#times').length) ) {
+      return false;
+    }
+    if (event.key === 'Escape') {
+      $('#times').parent().fadeOut(600, function() {
+        $(this).remove();
+      });
+    }
   });
 
 });
