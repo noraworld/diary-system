6d5aa1dea4ae9088a3fb1c4c46686ad38a8b033f
noraworld <noraworld@mail.com>
2017-03-27 23:12:25 +0900

    :pencil2: Insert a image string to cursor position

--- app/assets/javascripts/particular/upload.js
+++ app/assets/javascripts/particular/upload.js
@@ -24,15 +24,42 @@ $(function() {
         var date = new Date();
         // 5時間前(午前5時までは前の日の日付で保存される)
         date.setMinutes(date.getMinutes() - 60 * 5);
-        var year = date.getFullYear() + ''
+        var year = date.getFullYear() + '';
         var month = ('0' + (date.getMonth() + 1)).slice(-2);
+        var image = '![' + filename + '](/images/' + year + '/' + month + '/' + filepath + ')';
+        var sentence = getInsertionSentence(image);
 
-        textarea.val(textarea.val() + '![' + filename + '](/images/' + year + '/' + month + '/' + filepath + ')\n\n');
+        textarea.val(sentence);
       });
     },
     renameFilename: function(filename) {
       return filename.toLowerCase();
     },
   });
+
+  // insert a image string to cursor position
+  function getInsertionSentence(image) {
+    var textarea = document.querySelector('#post-content');
+
+    // release selection if some letters in textarea are selected
+    if (textarea.selectionStart !== textarea.selectionEnd) {
+      textarea.selectionEnd = textarea.selectionStart;
+    }
+
+    var sentence = textarea.value;
+    var len = sentence.length;
+    var pos = textarea.selectionStart;
+    var before = sentence.substr(0, pos);
+    var after = sentence.substr(pos, len);
+
+    var sentence = before + image + after;
+
+    // append a new line if the cursor position locates at the last line
+    if (pos === len) {
+      sentence += '\n\n';
+    }
+
+    return sentence;
+  }
 
 });
