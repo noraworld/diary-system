0d4a372eb581915a15adabb01bb0056b419b8d41
noraworld <noraworld@mail.com>
2018-03-04 07:08:31 +0900

    Update YouTube, Twitter and Mastodon iframe styles

--- app/assets/javascripts/common/articles.js
+++ app/assets/javascripts/common/articles.js
@@ -3,7 +3,16 @@ $(function() {
   // 画像を囲む画像リンクを削除する
   $('a').children('img').unwrap();
 
-  // YouTubeの埋め込みタグをdivで囲む
-  $('iframe').wrap('<div class="video-player-wrapper">');
+  // YouTube などの埋め込みタグを div で囲む
+  // videoDomains にドメインを追加することによって
+  // YouTube の埋め込みと同様のスタイルを適用できる
+  var videoDomains = [
+    'www.youtube.com',
+    'www.youtube-nocookie.com',
+  ];
+  for (var i = 0; i < videoDomains.length; i++) {
+    $('iframe[src*="http://' + videoDomains[i] + '"]').wrap('<div class="video-player-wrapper">');
+    $('iframe[src*="https://' + videoDomains[i] + '"]').wrap('<div class="video-player-wrapper">');
+  }
 
 });
