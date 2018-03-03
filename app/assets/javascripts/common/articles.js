ac4d8e5fc052c6c9a5e78f0f93a2ac07a4765c64
noraworld <noraworld@mail.com>
2018-03-04 05:42:26 +0900

    Delete target="_blank" of my own domains

--- app/assets/javascripts/common/articles.js
+++ app/assets/javascripts/common/articles.js
@@ -6,16 +6,4 @@ $(function() {
   // YouTubeの埋め込みタグをdivで囲む
   $('iframe').wrap('<div class="video-player-wrapper">');
 
-  // 自分が所有するドメインは現在のタブで開きリファラを有効にする
-  var myDomain = [
-    'noraworld.jp',
-    'blog.noraworld.jp',
-    'noraworld.blog',
-    'diary.noraworld.jp'
-  ];
-  for (var i = 0; i < myDomain.length; i++) {
-    $('a[href*="http://'  + myDomain[i] + '"]').removeAttr('target rel');
-    $('a[href*="https://' + myDomain[i] + '"]').removeAttr('target rel');
-  }
-
 });
