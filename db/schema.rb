752ae7399e714f25c14fbb92bd433df09ba28db0
noraworld <mail@noraworld.com>
2018-09-24 21:26:19 +0900

    Trim uninfluential spaces in db/schema.rb   - automatically changed this file with rails db:migrate in Rails 5.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,36 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219045753) do
+ActiveRecord::Schema.define(version: 2018_02_19_045753) do
 
   create_table "articles", force: :cascade do |t|
-    t.text     "text",       null: false
+    t.text "text", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "year",       null: false
-    t.integer  "month",      null: false
-    t.integer  "day",        null: false
-    t.date     "date",       null: false
+    t.integer "year", null: false
+    t.integer "month", null: false
+    t.integer "day", null: false
+    t.date "date", null: false
     t.index ["year", "month", "day"], name: "date_unique", unique: true
   end
 
   create_table "sample_articles", force: :cascade do |t|
-    t.text     "text"
-    t.integer  "year"
-    t.integer  "month"
-    t.integer  "day"
-    t.date     "date"
+    t.text "text"
+    t.integer "year"
+    t.integer "month"
+    t.integer "day"
+    t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "kind"
+    t.string "kind"
     t.index ["year", "month", "day"], name: "sample_articles_date_unique", unique: true
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
