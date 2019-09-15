419179e326eff8df899cdbaa9964b6e312fcf215
noraworld <mail@noraworld.com>
2019-09-15 19:20:53 +0900

    Format config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,23 @@ Rails.application.routes.draw do
   post   'login'  => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
-  get    '/:year/:month'              => 'articles#index',   constraints: {year: /20[0-9][0-9]/, month: /0[1-9]|1[0-2]/}
-  get    '/:year/:month/:day'         => 'articles#show',    constraints: {year: /20[0-9][0-9]/, month: /0[1-9]|1[0-2]/, day: /0[1-9]|[1-2][0-9]|3[0-1]/}
-  get    '/edit/:year/:month/:day'    => 'articles#edit',    constraints: {year: /20[0-9][0-9]/, month: /0[1-9]|1[0-2]/, day: /0[1-9]|[1-2][0-9]|3[0-1]/}, as: 'edit'
-  delete '/destroy/:year/:month/:day' => 'articles#destroy', constraints: {year: /20[0-9][0-9]/, month: /0[1-9]|1[0-2]/, day: /0[1-9]|[1-2][0-9]|3[0-1]/}, as: 'destroy'
-  get    '/new'                       => 'articles#new'
-  post   '/new'                       => 'articles#create'
-  post   '/:year/:month/:day'         => 'articles#update',  constraints: {year: /20[0-9][0-9]/, month: /0[1-9]|1[0-2]/, day: /0[1-9]|[1-2][0-9]|3[0-1]/}
-  patch  '/:year/:month/:day'         => 'articles#update',  constraints: {year: /20[0-9][0-9]/, month: /0[1-9]|1[0-2]/, day: /0[1-9]|[1-2][0-9]|3[0-1]/}
-  get    '/search'                    => 'articles#search'
-
+  get    '/:year/:month' => 'articles#index',
+         constraints: {year: /20[0-9][0-9]/, month: /0[1-9]|1[0-2]/}
+  get    '/:year/:month/:day' => 'articles#show',
+         constraints: {year: /20[0-9][0-9]/, month: /0[1-9]|1[0-2]/, day: /0[1-9]|[1-2][0-9]|3[0-1]/}
+  get    '/new' => 'articles#new'
+  post   '/new' => 'articles#create'
+  get    '/edit/:year/:month/:day' => 'articles#edit',
+         constraints: {year: /20[0-9][0-9]/, month: /0[1-9]|1[0-2]/, day: /0[1-9]|[1-2][0-9]|3[0-1]/},
+         as: 'edit'
+  post   '/:year/:month/:day' => 'articles#update',
+         constraints: {year: /20[0-9][0-9]/, month: /0[1-9]|1[0-2]/, day: /0[1-9]|[1-2][0-9]|3[0-1]/}
+  patch  '/:year/:month/:day' => 'articles#update',
+         constraints: {year: /20[0-9][0-9]/, month: /0[1-9]|1[0-2]/, day: /0[1-9]|[1-2][0-9]|3[0-1]/}
+  delete '/destroy/:year/:month/:day' => 'articles#destroy',
+         constraints: {year: /20[0-9][0-9]/, month: /0[1-9]|1[0-2]/, day: /0[1-9]|[1-2][0-9]|3[0-1]/},
+         as: 'destroy'
+  get    '/search' => 'articles#search'
   post   '/upload' => 'uploads#upload'
 
   get    '/templates'       => 'templates#index'
