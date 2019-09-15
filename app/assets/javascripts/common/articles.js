0263b68f858e57f99393e2ee6f8a7fabaade34c7
noraworld <mail@noraworld.com>
2019-09-15 22:43:11 +0900

    Enable headline fragment to scroll smoothly

--- app/assets/javascripts/common/articles.js
+++ app/assets/javascripts/common/articles.js
@@ -15,17 +15,46 @@ $(function() {
     $('iframe[src*="https://' + videoDomains[i] + '"]').wrap('<div class="video-player-wrapper">');
   }
 
+  // MEMO: jQuery 版フラグメントスクロールだとなぜか日本語のフラグメント識別子 (日本語の headline) に対しては
+  // Syntax error, unrecognized expression になってしまうので
+  // jQuery なしのバージョンでも同じ動作を行う
+  fragments = document.querySelectorAll('#post-content a[href^="#"], #templated-post-body a[href^="#"]');
+  for (var i = 0; i < fragments.length; i++) {
+    fragments[i].addEventListener('click', function(event) {
+
+      // h1 〜 h6 のフラグメント識別子をクリックしたときは URL を書き換えるが
+      // 注釈リンクをクリックしたときは URL を書き換えない
+      if (!this.hash.match(/^#fn(ref)?[0-9]+/g)) {
+        history.pushState(null, null, this.hash);
+      }
+
+      const rect      = this.getBoundingClientRect();
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      const position  = rect.top + scrollTop + getOffset();
+
+      scrollWithAnimation(event, position);
+    });
+  }
+
+  // jQuery 版フラグメントスクロール
   $('#post-content a[href^="#"]').on('click', function(event) {
+    const position = $(this.hash).offset().top + getOffset();
+
+    scrollWithAnimation(event, position);
+  });
+
+  function getOffset() {
+    return -10;
+  }
+
+  function scrollWithAnimation(event, position) {
     event.preventDefault();
 
-    var target     = $(this.hash);
-    var offset     = -10;
-    var position   = target.offset().top + offset;
-    var properties = {scrollTop: position};
-    var duration   = 500;
-    var easing     = 'swing';
+    const properties = { scrollTop: position };
+    const duration   = 350;
+    const easing     = 'swing';
 
     $('html, body').stop().animate(properties, duration, easing);
-  });
+  }
 
 });
