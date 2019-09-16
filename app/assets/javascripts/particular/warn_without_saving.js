00f9e2be0adb4d83a0b48c1cb6a05bb6f04eff0d
noraworld <mail@noraworld.com>
2019-09-16 10:31:24 +0900

    Warn without saving templated posts

--- app/assets/javascripts/particular/warn_without_saving.js
+++ app/assets/javascripts/particular/warn_without_saving.js
@@ -1,8 +1,25 @@
 $(function() {
 
-  var contents = $('#post-content').val();
+  var contents = {};
+  document.querySelectorAll('.templated-post-body').forEach(function(element, index) {
+    contents['templated-post-body-' + index] = element.value;
+  });
+  contents['post-content'] = document.querySelector('#post-content').value;
+
   window.onbeforeunload = function() {
-    if (contents !== $('#post-content').val()) {
+    editingFlag = false;
+
+    document.querySelectorAll('.templated-post-body').forEach(function(element, index) {
+      if (element.value !== contents['templated-post-body-' + index]) {
+        editingFlag = true;
+      }
+    });
+
+    if (document.querySelector('#post-content').value !== contents['post-content']) {
+      editingFlag = true;
+    }
+
+    if (editingFlag) {
       return false;
     }
   }
