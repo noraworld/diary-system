5082b829f6954d80cb05370bbccd14051e61c4bc
noraworld <mail@noraworld.com>
2019-09-16 11:30:31 +0900

    Remove flash message automatically

--- app/assets/javascripts/particular/close_alert_message.js
+++ app/assets/javascripts/particular/close_alert_message.js
@@ -1,25 +1,10 @@
 $(function() {
 
-  // Click a cross button to close.
-  $('#times').on('click', function() {
-    $(this).parent().fadeOut(600, function() {
+  timesRemoveTimeoutID = window.setTimeout(function() {
+    $('#times').parent().fadeOut(600, function() {
       $(this).remove();
     });
-  });
-  // Press esc key to close.
-  window.addEventListener('keydown', function(event) {
-    if ((document.activeElement.nodeName === 'INPUT'
-    || document.activeElement.nodeName === 'TEXTAREA'
-    || document.activeElement.getAttribute('type') === 'text')
-    || document.activeElement.isContentEditable === true
-    || !($('#times').length) ) {
-      return false;
-    }
-    else if (event.key === 'Escape') {
-      $('#times').parent().fadeOut(600, function() {
-        $(this).remove();
-      });
-    }
-  });
+    window.clearTimeout(timesRemoveTimeoutID);
+  }, 2000);
 
 });
