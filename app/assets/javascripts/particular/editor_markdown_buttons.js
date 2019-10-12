54b71ed0562fb57ea9e487686c97586c77149beb
noraworld <mail@noraworld.com>
2019-10-12 12:59:52 +0900

    Add ability to click listify button to listify   - delete shortcut to type ctrl to listify

--- app/assets/javascripts/particular/editor_markdown_buttons.js
+++ app/assets/javascripts/particular/editor_markdown_buttons.js
@@ -1,5 +1,89 @@
 'use strict';
 
 {
+  document.querySelector('#listify').addEventListener('click', (event) => {
+    if (getActiveTextarea() === null) {
+      return false;
+    }
 
+    let activeTextarea = getActiveTextarea();
+    let sentence = activeTextarea.value;
+    let selection = { start: activeTextarea.selectionStart, end: activeTextarea.selectionEnd };
+
+    activeTextarea.focus();
+
+    let isSelectionStartFirstLine = true;
+    for (let i = selection.start - 1; i >= 0; i--) {
+      if (sentence[i] === '\r' || sentence[i] === '\n') {
+        activeTextarea.selectionStart = i + 1;
+        isSelectionStartFirstLine = false;
+        break;
+      }
+    }
+    if (isSelectionStartFirstLine === true) {
+      activeTextarea.selectionStart = 0;
+    }
+
+    let isSelectionEndLastLine = true;
+    for (let i = selection.end; i < sentence.length; i++) {
+      if (sentence[i] === '\r' || sentence[i] === '\n') {
+        activeTextarea.selectionEnd = i;
+        isSelectionEndLastLine = false;
+        break;
+      }
+    }
+    if (isSelectionEndLastLine === true) {
+      activeTextarea.selectionEnd = sentence.length;
+    }
+
+    let lines = sentence
+                  .substr(activeTextarea.selectionStart, activeTextarea.selectionEnd - activeTextarea.selectionStart)
+                  .replace(/\r\n|\r/g, "\n")
+                  .split('\n');
+
+    for (let i = 0; i < lines.length; i++) {
+      // trim empty line
+      // this contains only spaces line
+      if (lines[i].replace(/ /g, '') === '') {
+        continue;
+      }
+
+      for (let j = 0; j < lines[i].length; j++) {
+        if (lines[i][j] !== ' ') {
+          if (j === 0) {
+            lines[i] = '- ' + lines[i];
+          }
+          else {
+            lines[i] = lines[i].slice(0, j) + '- ' + lines[i].slice(j);
+          }
+          break;
+        }
+      }
+    }
+
+    document.execCommand('insertText', false, lines.join('\n'));
+  });
+
+  let activeElementBeforeClickingListify = null;
+  document.querySelector('#listify').addEventListener('mouseover', (event) => {
+    activeElementBeforeClickingListify = document.activeElement;
+  });
+
+  function getActiveTextarea() {
+    if (activeElementBeforeClickingListify !== null && activeElementBeforeClickingListify.getAttribute('id') === 'post-content') {
+      return activeElementBeforeClickingListify;
+    }
+    else if (activeElementBeforeClickingListify !== null && activeElementBeforeClickingListify.getAttribute('class') === 'templated-post-body') {
+      return activeElementBeforeClickingListify;
+    }
+    else if (document.activeElement.getAttribute('class') === 'templated-post-body') {
+      return document.activeElement;
+    }
+    else if (document.activeElement.getAttribute('id') === 'post-content') {
+      return document.activeElement;
+    }
+    else {
+      return null;
+    }
+  }
 }
--- app/assets/javascripts/particular/editor_shortcuts.js
+++ app/assets/javascripts/particular/editor_shortcuts.js
@@ -65,68 +65,6 @@
         document.activeElement.scrollTop = document.activeElement.scrollHeight;
       }
     }
-    // TODO: add listify button to editor and move this feature
-    else if (event.key === 'Control') {
-      for (let i = selectionStartPosition - 1; i >= 0; i--) {
-        if (sentence[i] === '\r' || sentence[i] === '\n') {
-          document.activeElement.selectionStart = i + 1;
-          break;
-        }
-      }
-
-      let lines = sentence.substr(document.activeElement.selectionStart, document.activeElement.selectionEnd - document.activeElement.selectionStart).replace(/\r\n|\r/g, "\n").split('\n');
-      for (let i = 0; i < lines.length; i++) {
-        if (lines[i].replace(/ /g, '') === '') {
-          continue;
-        }
-
-        for (let j = 0; j < lines[i].length; j++) {
-          if (lines[i][j] !== ' ') {
-            if (j === 0) {
-              lines[i] = '- ' + lines[i];
-            }
-            else {
-              lines[i] = lines[i].slice(0, j) + '- ' + lines[i].slice(j);
-            }
-            break;
-          }
-        }
-      }
-      document.execCommand('insertText', false, lines.join('\n'));
-
-      // let selectionEndPosition = document.activeElement.selectionEnd;
-      // let hyphenInsertionPositions = [];
-
-      // for (let i = selectionStartPosition - 1; i >= 0; i--) {
-      //   if (sentence[i] === ' ') {
-      //     hyphenInsertionPositions.push(i + 1);
-      //     break;
-      //   }
-      // }
-      // if (hyphenInsertionPositions.length === 0) {
-      //   hyphenInsertionPositions.push(0);
-      // }
-
-      // for (let i = selectionStartPosition - 1; i < selectionEndPosition; i++) {
-      //   if (sentence[i] === '\n' || sentence[i] === '\r') {
-      //     do {
-      //       i++;
-      //     } while (sentence[i] === ' ' || i > document.activeElement.value.length);
-
-      //     hyphenInsertionPositions.push(i);
-      //   }
-      // }
-
-      // hyphenInsertionPositions = hyphenInsertionPositions.map((_element, index, _array) => {
-      //   return hyphenInsertionPositions[index] + index * 2;
-      // });
-
-      // for (let i = 0; i < hyphenInsertionPositions.length; i++) {
-      //   document.activeElement.selectionStart = hyphenInsertionPositions[i];
-      //   document.activeElement.selectionEnd   = hyphenInsertionPositions[i];
-      //   document.execCommand('insertText', false, '- ');
-      // }
-    }
   });
 
   function isFormFocused() {
