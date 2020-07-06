8de2e0174a0adeb4bd37a8eb600e6889318e3332
noraworld <mail@noraworld.com>
2020-07-06 19:22:30 +0900

    Make diary migration error message strictly

--- app/forms/migrate_form.rb
+++ app/forms/migrate_form.rb
@@ -6,10 +6,10 @@ class MigrateForm
   attr_accessor :from, :to
 
   validates :from, presence: { message: 'The origin date ("from" parameter) is missing.' }
-  validates :from, format: { with: /\A[0-9]{4}-[0-9]{2}-[0-9]{2}\z/, message: 'The "from" parameter is out of range of date type' }
+  validates :from, format: { with: /\A[0-9]{4}-[0-9]{2}-[0-9]{2}\z/, message: 'The "from" parameter is out of range of date type' }, allow_nil: true
 
   validates :to, presence: { message: 'The destination date ("to" parameter) is missing.' }
-  validates :to, format: { with: /\A[0-9]{4}-[0-9]{2}-[0-9]{2}\z/, message: 'The "to" parameter is out of range of date type' }
+  validates :to, format: { with: /\A[0-9]{4}-[0-9]{2}-[0-9]{2}\z/, message: 'The "to" parameter is out of range of date type' }, allow_nil: true
 
   validate :check_origin_existence
   validate :check_destination_existence
@@ -17,14 +17,14 @@ class MigrateForm
   private
 
   def check_origin_existence
-    return if from.blank?
+    return if from&.match(/\A[0-9]{4}-[0-9]{2}-[0-9]{2}\z/).nil?
 
     year_from, month_from, day_from = from.split('-').map(&:to_i)
     errors.add(:from, message: 'The diary not found in the origin date.') if Article.find_by(date: Date.new(year_from, month_from, day_from)).blank?
   end
 
   def check_destination_existence
-    return if to.blank?
+    return if to&.match(/\A[0-9]{4}-[0-9]{2}-[0-9]{2}\z/).nil?
 
     year_to, month_to, day_to = to.split('-').map(&:to_i)
     errors.add(:to, message: 'The diary already exists in the destination date.') if Article.find_by(date: Date.new(year_to, month_to, day_to)).present?
