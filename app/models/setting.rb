0631e37e4f746c32f2f34d122833f1e5a263fc27
noraworld <mail@noraworld.com>
2020-07-12 10:34:06 +0900

    Add next day adjustment hour

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -8,6 +8,15 @@ class Setting < ApplicationRecord
             },
             allow_nil: true
 
+  validates :next_day_adjustment_hour,
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 0,
+              less_than_or_equal_to: 23,
+              message: 'should be greater than or equal to 0 and less than or equal to 23'
+            },
+            allow_nil: false
+
   validates :ga_tracking_identifier,
             format: {
               with: /\AUA-[0-9]{9}-[0-9]{1}\z/,
