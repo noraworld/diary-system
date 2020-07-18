166d976fd9781ff9a82dcdeb4affb47e92788466
noraworld <mail@noraworld.com>
2020-07-18 09:55:18 +0900

    Set time zone as environment variable - Use it to config.time_zone and adjusted_current_time - Force to set it and delete default value 'UTC'

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -2,6 +2,6 @@
 
 module ArticlesHelper
   def adjusted_current_time
-    Time.now.in_time_zone(ENV.fetch('TIME_ZONE', 'UTC')) - Setting.last.next_day_adjustment_hour.hours
+    Time.now.in_time_zone(ENV.fetch('TIME_ZONE')) - Setting.last.next_day_adjustment_hour.hours
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Diary
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-    config.time_zone = 'Tokyo' # delete if confirming no need
+    config.time_zone = ENV.fetch('TIME_ZONE')
     config.active_record.default_timezone = :local
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
