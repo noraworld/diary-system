ad4b52d5efd1695791a69313149277a670175cf2
noraworld <mail@noraworld.com>
2020-10-14 13:40:01 +0900

    Preview templated forms’ markdown Preview all markdowns in loading page

--- app/assets/javascripts/particular/preview_markdown.js
+++ app/assets/javascripts/particular/preview_markdown.js
@@ -1,33 +1,45 @@
 'use strict';
 
 {
+  const postContent         = document.querySelector('#textarea #post-content');
+  const templatedPostBodies = document.querySelectorAll('#textarea .templated-post-body');
+
+  preview(postContent);
+
+  postContent.addEventListener('input', preview);
+  for (var i = 0; i < templatedPostBodies.length; i++) {
+    preview(templatedPostBodies[i]);
+    templatedPostBodies[i].addEventListener('input', preview);
+  }
+}
+
+function preview(node = null) {
   const markdownEndpoint = '/api/v1/markdown';
+  const _this = this || node;
 
-  document.querySelector('#post-content').addEventListener('input', function() {
-    var obj = {};
-    obj['body'] = this.value;
-    obj['authenticity_token'] = $('meta[name="csrf-token"]').attr('content');
+  var obj = {};
+  obj['body'] = _this.value;
+  obj['authenticity_token'] = $('meta[name="csrf-token"]').attr('content');
 
-    const method = 'POST';
-    const headers = {
-      'Accept': 'application/json',
-      'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
-    };
-    const body = Object.keys(obj).map((key) => key + '=' + encodeURIComponent(obj[key])).join('&');
+  const method = 'POST';
+  const headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+  };
+  const body = Object.keys(obj).map((key) => key + '=' + encodeURIComponent(obj[key])).join('&');
 
-    fetch(
-      markdownEndpoint,
-      {
-        method,
-        headers,
-        body
-      }
-    ).then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-    }).then((data) => {
-      document.querySelector('#post-content-preview').innerHTML = data.markdown;
-    });
+  fetch(
+    markdownEndpoint,
+    {
+      method,
+      headers,
+      body
+    }
+  ).then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+  }).then((data) => {
+    _this.parentNode.children.namedItem('post-content-preview').innerHTML = data.markdown;
   });
 }
